cf767a4cf62beababac8a9ff952fe14527ccae51
timothy <timothy.ndarau@student.moringaschool.com>
2023-08-21 23:56:46 +0300

    countdown happening

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,11 @@
 # your code goes here!
+def countdown (number) :
+    while number > 0 :
+         
+         print(f'{number} SECOND(S)!')
+
+         number -= 1
+
+         print ("HAPPY NEW YEAR!")
+
+         countdown(10)
--- lib/debug.py
+++ lib/debug.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from countdown import countdown, countdown_with_sleep
+from lib.countdown import countdown, countdown_with_sleep
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
